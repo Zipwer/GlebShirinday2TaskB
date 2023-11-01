6cd540f0a9e8bc49be40e34ed329299fe22cd199
glebs <glebs@GLEB-PC>
2023-11-01 04:31:18 +0300

    Добавьте файлы проекта.

---
+++ GlebShirinday2TaskB/GlebShirinday2TaskB.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string abbreviateWord(const string& word) {
+    if (word.length() > 10) {
+        string abbreviation = word[0] + to_string(word.length() - 2) + word.back();
+        return abbreviation;
+    }
+    return word;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<string> words(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> words[i];
+    }
+
+    for (const string& word : words) {
+        cout << abbreviateWord(word) << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
